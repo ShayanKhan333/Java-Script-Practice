d4705891045373376240f284358c06c28021992e
Shayan Khan <121638447+ShayanKhan333@users.noreply.github.com>
2024-05-05 02:56:12 +0500

    Add files via upload

---
+++ Arrays/Practice 1/apps.js
@@ -0,0 +1,14 @@
+// let arr=["shayan","ayan","hafsa"]
+// console.log(arr)
+// arr.push("Maqssod","nasreen")   ya method last ma value ko add karta ha
+// arr.unshift("Maqssod","Nasreen")   ya method start ma value ko add karta ha
+// arr.pop() ya method end sa value ko nikal ta ha
+// arr.shift() ya method start sa value ko nikal ta ha
+//console.log(arr.slice(1,2)) is methode ma ya hota ha ka hum jo pheli index no  den ga tu wo os ko nhi dekha ayaa  ga or phir wahi sa wo count kara ga or jo hum dosra index no den ga tu wo first index sa dosra index tk value ko da ga or baki ko nhi da ga
+ //console.log(arr.splice(0,3))  is method ma hum ko do index no dena hota hm phela ya ka kaha sa or dorsa index no kaha tk phir is ko dekh kar value print hojati ha
+//  console.log(arr)
+
+let a=[ ]
+let value =prompt("enter your Name")
+a.push(value )
+document.write(a)
