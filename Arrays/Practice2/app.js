63b0dbf7094d6c7b341c709beeb5e9a82ef43a0a
Shayan Khan <121638447+ShayanKhan333@users.noreply.github.com>
2024-05-09 15:35:42 +0500

    Add files via upload

---
+++ Arrays/Practice2/app.js
@@ -0,0 +1,76 @@
+// let cars=["Honda","Toyota","lamborgani","Ferrari"];
+// cars[0]="BMW";
+// cars[2]="supra"
+// console.log(cars[1])
+// console.log(cars)
+
+// const mobile=["iphone","samsung"]
+// mobile[1]="Google pixel"
+// console.log(mobile)
+
+
+// array to String
+// const fruits=["banana","mango","apple"];
+// document.getElementById("demo").innerHTML=fruits.toString()
+
+// arrays second version
+//  const cars =new Array("supra","gtr","dodge challenger")
+
+//  console.log(cars)
+
+// Arrays two method 
+
+// let cars=["Honda","Toyota","lamborgani","Ferrari"];
+// console.log(cars.length) 
+// console.log(cars.sort())
+
+// Accessing the Last Array Element
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let lastfruitsele=fruits[fruits.length -2]
+// console.log(lastfruitsele)
+
+// Looping Array Elements
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let text = "<ul>";
+// for (let i = 0; i <fruits.length; i++) {
+//   text += "<li>" + fruits[i] + "</li>";
+// }
+// text += "</ul>";
+// agar hum is ma sa pluse hata den ga tu wo sirf ek dega wo bhi last wala
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let text="<ul>";
+// for (let i = 0; i <ul fruits.length; i++) {
+//     text +="<li>"+fruits[i]+"</li>";
+// }
+// text +="</ul>";
+// document.getElementById("demo").innerHTML = text;
+
+// For Each function Method
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let text ="<ul>";
+// fruits.forEach(myFunction)
+
+// text+="</ul>";
+// document.getElementById("demo").innerHTML = text;
+// function myFunction(value) {
+//     text += "<li>" + value + "</li>"
+// }
+// Push Method
+// const fruits = ["Banana", "Orange", "Apple"];
+// fruits.push("lemon")
+// console.log(fruits)
+
+// SECOND METHOD ARRAY MA PUSH KARNA KA LIYA
+// const fruits = ["Banana", "Orange", "Apple"];
+// fruits[fruits.length] = "Lemon";
+
+// type of
+
+// const fruits = ["Banana", "Orange", "Apple"];
+// const type=typeof(fruits)
+// console.log(type)
+
+// array ki type object hoti hai
