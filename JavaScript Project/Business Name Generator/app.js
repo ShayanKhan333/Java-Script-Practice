6d509a62d6cceef63d5bed8fe1df5699d648c209
Shayan Khan <121638447+ShayanKhan333@users.noreply.github.com>
2024-05-13 15:35:29 +0500

    Add files via upload

---
+++ JavaScript Project/Business Name Generator/app.js
@@ -0,0 +1,34 @@
+// Adjective
+// a="Fire"
+// b="Great"
+// c="Crazy"
+
+// ShopName
+// a="Foods"
+// b="Salon"
+// c="Garments"
+
+// AnotherName
+// a="Limited"
+// b="PVT"
+// c="Bros"
+//JavaScript Code
+// function Generate ()
+// {
+//     var adj =  ["Fire ", "Great ", "Crazy "],
+//         shp =  ["Foods ", "Salon ", "Garments "],
+//         aeu =  ["Limited","PVT","Bros"];
+
+
+
+//     var a = 2*Math.random (), b = 2*Math.random (), c = 2*Math.random ();
+//     a = Math.floor(a);
+//     b = Math.floor(b);
+//     c = Math.floor(c);
+//     var adjgen = adj [a], shpgen= shp [b], aeugen = aeu [c];
+
+//     var FullName = adjgen + shpgen + aeugen;
+//     console.log(FullName)
+//     var namespace = document.getElementById ("Name-space");
+//     namespace.innerHTML = "<p>" + FullName + "</p>";
+// }
